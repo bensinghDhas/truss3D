21e1a3fb431de150d0e7480a4ee6e514309626bf
bensinghDhas <bensinghdhas@iisc.ac.in>
2022-10-27 22:31:34 +0530

    added plot method to the truss3D object

--- examples/t3DTestInput.m
+++ examples/t3DTestInput.m
@@ -2,6 +2,7 @@ close all;
 clear al;
 clc;
 addpath("..\src\");
-nodes=[0 0 0; 1 0 0;2 0 0];
+nodes=[0 0 0; 1 0 0;2 1 0];
 edges=[1 2;2 3];
 truss=truss3D(nodes,edges);
+truss.plot(1)
--- src/truss3D.m
+++ src/truss3D.m
@@ -6,6 +6,7 @@ classdef truss3D< handle
         noNd
     end
     methods
+        %%Constructor
         function obj=truss3D(nodes, edges)
             if (size(nodes,2)==3 && size(edges,2)==2)
                 obj.nodes=nodes;
@@ -15,6 +16,17 @@ classdef truss3D< handle
             else
                 error("Invalid input: Points are not in 3D")
             end
+        end
+
+        %%plotting functions
+        function plot(obj,figId)
+            figure(figId)
+            hold on;
+            for itEg=1:obj.noEl
+                tmp=obj.nodes(obj.edges(itEg,:),:);
+                plot3(tmp(:,1),tmp(:,2),tmp(:,3),'-ob', LineWidth=2, markerFaceColor='k');
+            end
+            hold off
         end
     end
 end
